1dec0927da2e459c328bb3bf7015f725da6b2c4e
Sravani Dodla <sravanidodla@users.noreply.github.com>
2020-02-13 12:52:56 -0500

    Added Docker part 2 screenshots

---
+++ dockerpart2/Fibonacci.sh
@@ -0,0 +1,19 @@
+
+#!/bin/bash
+
+## Static Input Number
+NUM=$1
+
+a=0
+b=1
+
+echo "Fibonacci Series is - "
+
+for (( i=0; i<NUM; i++ ))
+do
+    echo -n "$a "
+    fibo=$(( a + b ))
+    a=$b
+    b=$fibo
+done
+
